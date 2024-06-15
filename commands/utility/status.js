ced7a05afcfbc980df9d421571eecc3ab929b884
Fafikk <kubamichalowski17@gmail.com>
2024-06-15 15:00:25 +0200

    updated status command

--- commands/utility/status.js
+++ commands/utility/status.js
@@ -10,8 +10,38 @@ module.exports = {
 
         function check_minecraft() {
             return new Promise((resolve, reject) => {
-                const ip = '192.168.2.11';
+                const ip = '';
                 const port = 25565;
+        
+                const socket = new net.Socket();
+                const startTime = Date.now();
+        
+                socket.setTimeout(2500);
+        
+                socket.on('connect', () => {
+                    const endTime = Date.now();
+                    const responseTime = endTime - startTime;
+                    socket.destroy();
+                    resolve(responseTime);
+                });
+        
+                socket.on('timeout', () => {
+                    socket.destroy();
+                    reject(new Error('timeout'));
+                });
+        
+                socket.on('error', (err) => {
+                    reject(err);
+                });
+        
+                socket.connect(port, ip);
+            });
+        }
+
+        function check_site() {
+            return new Promise((resolve, reject) => {
+                const ip = '';
+                const port = 80;
         
                 const socket = new net.Socket();
                 const startTime = Date.now();
@@ -40,13 +70,22 @@ module.exports = {
 
         let desc = "";
         let color;
+
+        try {
+            const responseTime = await check_site();
+            desc = `Strona internetowa: :white_check_mark: - ${responseTime} ms \n`;
+            color = 0x00FF00;
+        } catch (error) {
+            desc = "Strona internetowa: :x: - Przekroczono czas oczekiwania. \n";
+            color = 0xFF0000;
+        }
         
         try {
             const responseTime = await check_minecraft();
-            desc = `Minecraft: :white_check_mark: - ${responseTime} ms`;
+            desc += `Minecraft: :white_check_mark: - ${responseTime} ms`;
             color = 0x00FF00;
         } catch (error) {
-            desc = "Minecraft: :x: - Przekroczono czas oczekiwania.";
+            desc += "Minecraft: :x: - Przekroczono czas oczekiwania.";
             color = 0xFF0000;
         }
 
